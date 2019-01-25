9596f2f692127c884999032fdcdb56771d46289f
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-25 16:31:09 +0000

    blur images

--- app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/MainActivity.java
@@ -6,7 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.jgabrielfreitas.core.BlurImageView;
+
 import robfernandes.xyz.go4lunch.R;
+
+import static robfernandes.xyz.go4lunch.utils.Constants.BLUR_RADIOUS;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,7 +22,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setViews();
+        blurBackground();
         setOnClickListeners();
+    }
+
+    private void blurBackground() {
+        BlurImageView blurImageView = findViewById(R.id.activity_main_background_blur_image);
+        blurImageView.setBlur(BLUR_RADIOUS);
     }
 
     private void setViews() {
---
+++ app/src/main/java/robfernandes/xyz/go4lunch/utils/Constants.java
@@ -0,0 +1,8 @@
+package robfernandes.xyz.go4lunch.utils;
+
+/**
+ * Created by Roberto Fernandes on 25/01/2019.
+ */
+public final class Constants {
+    public static final int BLUR_RADIOUS = 5;
+}
