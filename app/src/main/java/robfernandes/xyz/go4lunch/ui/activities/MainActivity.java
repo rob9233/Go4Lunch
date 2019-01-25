df83370963324c07967a039c2987b1cb8d3b5a65
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-25 20:17:19 +0000

    WIP - Register Activity

--- app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.jgabrielfreitas.core.BlurImageView;
 
@@ -15,6 +16,7 @@ import static robfernandes.xyz.go4lunch.utils.Constants.BLUR_RADIOUS;
 public class MainActivity extends AppCompatActivity {
 
     private Button logInBtn;
+    private TextView dontHaveAccountTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +35,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void setViews() {
         logInBtn = findViewById(R.id.activity_main_log_in_btn);
+        dontHaveAccountTextView = findViewById(R.id.activity_main_dont_have_account_text_view);
     }
 
     private void setOnClickListeners() {
         logInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this
-                        , NavigationActivity.class);
-                //to not come back to this intent when pressing back button
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                goToActivityWithClearTask( new Intent(MainActivity.this
+                        , NavigationActivity.class));
             }
         });
+
+        dontHaveAccountTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            }
+        });
+    }
+
+    private void goToActivityWithClearTask(Intent intent) {
+        //to not come back to this intent when pressing back button
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
