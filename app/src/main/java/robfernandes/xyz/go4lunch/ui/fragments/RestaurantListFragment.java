78633ea49cdb1b6e594f2d346d0dfa7644e5b2b0
Roberto Fernandes <roberto.fernandes21@gmail.com>
2019-01-28 19:18:49 +0000

    constants change

--- app/src/main/java/robfernandes/xyz/go4lunch/ui/fragments/RestaurantListFragment.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/ui/fragments/RestaurantListFragment.java
@@ -19,7 +19,6 @@ public class RestaurantListFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
