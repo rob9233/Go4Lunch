c2188a0e7c88e255702bf86a627b3af6039165e9
roberto.fernandes <roberto.fernandes@timwetech.com>
2019-05-31 11:38:52 +0100

    log in implementation

--- app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/MainActivity.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/MainActivity.java
@@ -2,10 +2,17 @@ package robfernandes.xyz.go4lunch.ui.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -16,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Button logInWithEmailBtn;
     private Button logInWithFacebookBtn;
     private TextView dontHaveAccountTextView;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,15 +37,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStart() {
         super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser!= null) {
+        if (currentUser != null) {
             goToActivityWithClearTask(new Intent(MainActivity.this
                     , NavigationActivity.class));
         }
     }
 
     private void setViews() {
+        email = findViewById(R.id.activity_main_email_edit_text);
+        password = findViewById(R.id.activity_main_password_edit_text);
         logInWithEmailBtn = findViewById(R.id.activity_main_log_in_btn);
         dontHaveAccountTextView = findViewById(R.id.activity_main_dont_have_account_text_view);
         logInWithFacebookBtn = findViewById(R.id.activity_main_facebook_btn);
@@ -49,14 +58,37 @@ public class MainActivity extends AppCompatActivity {
                         , NavigationActivity.class))
         );
 
-        logInWithEmailBtn.setOnClickListener(v ->
-                startActivity(new Intent(MainActivity.this
-                        , LoginInEmailActivity.class))
+        logInWithEmailBtn.setOnClickListener(v -> {
+                    if (email.getText().toString().isEmpty()) {
+                        Toast.makeText(getBaseContext(), "please insert an email",
+                                Toast.LENGTH_SHORT).show();
+                    } else if (password.getText().toString().isEmpty()) {
+                        Toast.makeText(getBaseContext(), "please insert a password",
+                                Toast.LENGTH_SHORT).show();
+                    } else {
+                        logInWithEmail();
+                    }
+                }
         );
 
         dontHaveAccountTextView.setOnClickListener(v -> {
-            //    startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
+            startActivity(new Intent(MainActivity.this
+                    , RegisterEmailActivity.class));
         });
+    }
+
+    private void logInWithEmail() {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email.getText().toString()
+                , password.getText().toString())
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        goToActivityWithClearTask(new Intent(MainActivity.this
+                                , NavigationActivity.class));
+                    } else {
+                        Toast.makeText(getBaseContext(), "login failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
     private void goToActivityWithClearTask(Intent intent) {
--- app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/RegisterEmailActivity.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/ui/activities/RegisterEmailActivity.java
@@ -1,24 +1,17 @@
 package robfernandes.xyz.go4lunch.ui.activities;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 import robfernandes.xyz.go4lunch.R;
 
-public class LoginInEmailActivity extends AppCompatActivity {
+public class RegisterEmailActivity extends AppCompatActivity {
 
     private EditText emailEditText, passwordEditText;
     private Button registerBtn;
@@ -27,7 +20,7 @@ public class LoginInEmailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_in_email);
+        setContentView(R.layout.activity_register_email);
 
         setViews();
         mAuth = FirebaseAuth.getInstance();
@@ -35,9 +28,9 @@ public class LoginInEmailActivity extends AppCompatActivity {
     }
 
     private void setViews() {
-        emailEditText = findViewById(R.id.activity_log_in_email_email);
-        passwordEditText = findViewById(R.id.activity_log_in_email_password);
-        registerBtn = findViewById(R.id.activity_log_in_email_register_btn);
+        emailEditText = findViewById(R.id.activity_register_email_email);
+        passwordEditText = findViewById(R.id.activity_register_email_password);
+        registerBtn = findViewById(R.id.activity_register_email_register_btn);
     }
 
     private void setListeners() {
@@ -72,7 +65,7 @@ public class LoginInEmailActivity extends AppCompatActivity {
     }
 
     private void goToNavigationActivity() {
-        Intent intent = new Intent(LoginInEmailActivity.this
+        Intent intent = new Intent(RegisterEmailActivity.this
                 , NavigationActivity.class);
 
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
