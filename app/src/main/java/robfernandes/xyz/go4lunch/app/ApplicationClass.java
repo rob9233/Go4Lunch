25cc51e3095f08e1f7d2eece67e0c132f0ce5d88
roberto.fernandes <roberto.fernandes@timwetech.com>
2019-06-07 17:29:16 +0100

    show notifications

--- app/src/main/java/robfernandes/xyz/go4lunch/app/ApplicationClass.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/app/ApplicationClass.java
@@ -1,11 +1,29 @@
 package robfernandes.xyz.go4lunch.app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.support.multidex.MultiDexApplication;
 
 public class ApplicationClass extends MultiDexApplication {
+    public static final String CHANNEL_PLANS_ID = "CHANNEL_PLANS";
 
     @Override
     public void onCreate() {
         super.onCreate();
+        createNotificationChannels();
+    }
+
+    private void createNotificationChannels() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_PLANS_ID,
+                    "Show plans",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("Go4Lunch plans notifications");
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel1);
+        }
     }
 }
--- app/src/main/java/robfernandes/xyz/go4lunch/services/cloudMessaging/MessagingService.java
+++ app/src/main/java/robfernandes/xyz/go4lunch/services/cloudMessaging/MessagingService.java
@@ -1,5 +1,7 @@
 package robfernandes.xyz.go4lunch.services.cloudMessaging;
 
+import android.util.Log;
+
 import com.google.firebase.messaging.FirebaseMessagingService;
 
 public class MessagingService extends FirebaseMessagingService {
